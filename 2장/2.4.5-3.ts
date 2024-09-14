72b4ea70147873e0259e3a9cf9ad6ae9de53823e
park-my-1540 <parkmy722698@gmail.com>
2024-09-14 18:46:34 +0900

    type function ex 추가

--- 2장/2.4.5-3.ts
+++ 2장/2.4.5-3.ts
@@ -1,1 +1,12 @@
 type add = (a: number, b: number) => number;
+
+const adder:add = (x, y) => x + y;
+
+console.log(adder(2, 4));
+
+type Operation = (x : number, y : number) => number;
+function calculate(op: Operation, a: number, b: number):number {
+  return op(a, b);
+}
+const multiply:Operation = (x, y) => x * y;
+console.log(calculate(multiply, 4, 5));
