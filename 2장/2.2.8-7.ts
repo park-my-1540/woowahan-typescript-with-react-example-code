34ff9f9236408f3f813adde4fd86ef9285914e0c
park-my-1540 <parkmy722698@gmail.com>
2024-09-14 16:37:25 +0900

    타입과 변수는 다른 공간에 있으므로 이름이 중복되어도 됨.
    대신 eslint 에서 no-declare규칙을 정했다면 중복선언으로 에러 발생

--- 2장/2.2.8-7.ts
+++ 2장/2.2.8-7.ts
@@ -2,3 +2,9 @@ type Developer = { isWorking: true };
 const Developer = { isTyping: true }; // OK
 type Cat = { name: string; age: number };
 const Cat = { slideStuffOffTheTable: true }; // OK
+
+/**
+ * 타입스크립트 에러 발생하지 않음.
+ * "no-redeclare": "off"
+ * 그러나 eslint의 no-redeclare 규칙은 기본적으로 타입과 변수를 구분하지 않고 동일한 이름으로 선언된 경우를 중복 선언으로 간주할 수 있습니다
+ */
