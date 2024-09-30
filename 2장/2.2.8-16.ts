ebbfc28e87a362054f43c38e3dbc2b7129f67cf2
park-my-1540 <parkmy722698@gmail.com>
2024-09-30 18:08:08 +0900

    enum 대신 object 를 제너릭을 이용하여 유니온 타입으로 변경하기

--- 2장/2.2.8-16.ts
+++ 2장/2.2.8-16.ts
@@ -3,4 +3,28 @@ enum Direction {
   Down, // 1
   Left, // 2
   Right, // 3
-  }
+}
+
+const Status = {
+  pending: 0,
+  success: 1,
+  fail: 2
+} as const;
+
+const StatusArr = ["pending", "success", "fail"] as const;
+
+// Object
+type ObjectUnion<T extends {[key:string]: unknown}> = keyof T;
+type StatusType = ObjectUnion<typeof Status>
+
+// Array
+type ArrayUnion<T extends ReadonlyArray<any>> = T[number];
+type StatusArrType = ArrayUnion<typeof StatusArr>
+
+type ValueOfUnion<T> = T extends {[key:string]:unknown}
+                      ? ObjectUnion<T>
+                      : T extends ReadonlyArray<any>
+                      ? ArrayUnion<T>
+                      : never
+
+type test = ValueOfUnion<typeof Direction>
